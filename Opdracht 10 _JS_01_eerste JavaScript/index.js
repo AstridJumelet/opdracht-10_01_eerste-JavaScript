50be247f2777fd45c51cea267a174e513ec7ef2c
Astrid Jumelet <89905425+AstridJumelet@users.noreply.github.com>
2021-10-26 16:55:10 +0200

    Add files via upload

---
+++ Opdracht 10 _JS_01_eerste JavaScript/index.js
@@ -0,0 +1,22 @@
+//dit is mijn eerste JavaScript werkje
+console.log('Hello Winc Academy')
+let firstName = 'Astrid';
+let lastName = 'Jumelet';
+console.log('Astrid', 'Jumelet');
+//simpele oefening optellen
+let x = 4; // Maak een variabele, met de naam x, en deze wordt 4
+let y = 4;
+console.log(x + y)
+let xx = 8;
+let yy = 1;
+console.log(xx - yy);
+let xxx = 3;
+let yyy = 2;
+console.log(xxx * yyy);
+let xxxx = 10;
+let yyyy = 250;
+console.log(xxxx % yyyy);
+let leeftijd = '53';
+console.log('53');
+let leeftijd = 'drieenvijftig';
+console.log('drieenvijftig');
